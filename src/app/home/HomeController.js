6ed2b18d6529b8c9332d4cbf268ef8b5b2e01c26
Dale- <18717394542@163.com>
2016-12-11 20:58:29 +0800

    add evaluate list into evaluate area

--- src/app/home/HomeController.js
+++ src/app/home/HomeController.js
@@ -8,6 +8,49 @@ export default class HomeController {
 
 	constructor() {
 		this.name = 'Home';
+		this.evaluteDatas = [
+			{
+				'icon': 'https://cdn.forestry.io/assets/gulp/profile-picture-cheryl-aa258c1a6a0069de5abbc08ec3cdc70e7ad8cd10cadc51977d825bbdb02338da.jpg',
+				'commit': 'Update from Forestry.io - setting-up-site/uploading-a-zip-file.md',
+				'author': 'DaleDu',
+				'date': 'COMMITED WITH FORESTRY.IO 28 MINUTES AGO',
+				'commitId': '37CB018'
+			},
+			{
+				'icon': 'https://cdn.forestry.io/assets/gulp/profile-picture-scott-2c1551c9f71f45a97340bb651effe4b0a01f1faca33d1c6f20f902959fd5459b.jpg',
+				'commit': 'Merge branch: master of github.com:forestryio/docs',
+				'author': 'SCOTTGALLANT',
+				'date': 'COMMITED WITH FORESTRY.IO 40 MINUTES AGO',
+				'commitId': 'FB3AC1C'
+			},
+			{
+				'icon': 'https://cdn.forestry.io/assets/gulp/profile-picture-scott-2c1551c9f71f45a97340bb651effe4b0a01f1faca33d1c6f20f902959fd5459b.jpg',
+				'commit': 'Updated CSS fore large images',
+				'author': 'COTTGALLANT',
+				'date': 'COMMITED WITH FORESTRY.IO 42 MINUTES AGO',
+				'commitId': '25C4FD8'
+			},
+			{
+				'icon': 'https://cdn.forestry.io/assets/gulp/profile-picture-cheryl-aa258c1a6a0069de5abbc08ec3cdc70e7ad8cd10cadc51977d825bbdb02338da.jpg',
+				'commit': 'Update from Forestry.io - setting-up-site/uploading-a-zip-file.md',
+				'author': 'CHERYLJAM',
+				'date': 'COMMITED WITH FORESTRY.IO 48 MINUTES AGO',
+				'commitId': 'E9A02D3'
+			},
+			{
+				'icon': 'https://cdn.forestry.io/assets/gulp/profile-picture-cheryl-aa258c1a6a0069de5abbc08ec3cdc70e7ad8cd10cadc51977d825bbdb02338da.jpg',
+				'commit': 'Set max-width for images in post',
+				'author': 'SCOTTGALLANT',
+				'date': 'COMMITED WITH FORESTRY.IO AN HOUR AGO',
+				'commitId': '9C3BEB6'
+			},
+			{	'icon': 'https://cdn.forestry.io/assets/gulp/profile-picture-cheryl-aa258c1a6a0069de5abbc08ec3cdc70e7ad8cd10cadc51977d825bbdb02338da.jpg',
+				'commit': 'Merge branch : master of github.com:forestryio/docs',
+				'author': 'SCOTTGALLANT',
+				'date': 'COMMITED WITH FORESTRY.IO 2 HOURS AGO',
+				'commitId': '6D58E89'
+			}
+		];
 	}
 
 }
